4c87645fdf89138d78f8ef4044bf2956721170fc
Fearmipan <87984437+Fearmipan@users.noreply.github.com>
2023-10-28 07:02:50 +0700

    Update GDCC.sh
    Fix typo

--- GDCC.sh
+++ GDCC.sh
@@ -23,7 +23,7 @@ sleep 3
 echo '0.1|0.2|0.3|0.4|0.5|0.6|0.7|0.8|0.9'
 read DOWNSCALE
 sleep 2
-echo "Enter for downscale for Battery Saber Mode" 
+echo "Enter for downscale for Battery Saver Mode" 
 read DOWNSCALE2
 echo " "
 sleep 2
